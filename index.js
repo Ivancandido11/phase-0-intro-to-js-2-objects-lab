3f9e2a310485c4676895867d7cbe0a69186f1602
Ivan Candido <ivan.candido@powerhrg.com>
2021-05-24 15:53:25 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Write your solution in this file!
+const employee = {name : 'Ivan', address : 'Tampa'}
+
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    return Object.assign({}, obj, {[key] : value})
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    return Object.assign(obj, {[key] : value})
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newEmployee = {...obj}
+    delete newEmployee[key]
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    const newEmployee = obj
+    delete newEmployee[key]
+    return newEmployee
+}
